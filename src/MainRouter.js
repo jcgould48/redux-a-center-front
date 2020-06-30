99d984b6c239f1307037b2bb30d64c13aa765d53
jharte5 <jonathan.harte@codeimmersives.com>
2020-06-30 12:47:59 -0400

    added route for rental items

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Toastify from "./Components/Toastify/Toastify";
+// import PrivateRoute from './Components/PrivateRoute/PrivateRoute'
 
 const Signup = React.lazy(() => import("./Components/Signup/Signup"));
 const Navbar = React.lazy(() => import("./Components/Navbar/Navbar"));
 const Login = React.lazy(() => import("./Components/Login/Login"));
+// const RentalItems =  React.lazy(() => import ('./Components/RentalItems/RentalItems'))
 
 export default class MainRouter extends Component {
     render() {
@@ -15,7 +17,9 @@ export default class MainRouter extends Component {
                 <Switch>
                     <Route exact path='/sign-up' component={Signup} />
                     <Route exact path= '/login' component={Login} />
+                    {/* <PrivateRoute  exact path='/items' component={RentalItems}/> */}
                 </Switch>
+                <h1>Redux a Center</h1>
             </>
         )
     }
