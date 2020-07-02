d9c4df5d127e02e2a21f3c35b556dea2892dd8eb
Jesse Gould <jesse.gould@codeimmersives.com>
2020-07-02 14:29:20 -0400

    get all items and makes item cards

--- src/redux/actions/itemActions.js
+++ src/redux/actions/itemActions.js
@@ -1,6 +1,6 @@
 import Axios from "../lib/Axios/Axios";
 import setAuthToken from "../lib/Axios/setAuthToken"
-import {CREATE_ITEM} from "../constants/itemConstant"
+import {CREATE_ITEM, GET_ALL_ITEMS} from "../constants/itemConstant"
 
 
 export const createItem = (itemInfo) => async (dispatch) => {
@@ -12,6 +12,20 @@ export const createItem = (itemInfo) => async (dispatch) => {
     });
     return Promise.resolve();
   } catch (e) {
+    return Promise.reject(e.response.data.message);
+  }
+};
+
+export const getAllItems = () => async (dispatch) => {
+  try {
+    let success = await Axios.get("/api/items/all-rental-items");
+    // console.log("$$$$$", success)
+    dispatch({
+      type: GET_ALL_ITEMS,
+      payload: success.data,
+    });
+  } catch (e) {
+    console.log("ERROR", e)
     return Promise.reject(e.response.data.message);
   }
 };
--- src/redux/constants/itemConstant.js
+++ src/redux/constants/itemConstant.js
@@ -1,1 +1,2 @@
 export const CREATE_ITEM= 'CREATE_ITEM'
+export const GET_ALL_ITEMS= 'GET_ALL_ITEMS'
